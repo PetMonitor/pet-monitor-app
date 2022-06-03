012657ab61b447d83805026d0710728ab7b48bd4
Camila de Trincheria <cdetrincheria@medallia.com>
2022-06-02 22:42:50 -0300

    header and style fixes

--- App.js
+++ App.js
@@ -42,14 +42,14 @@ export default class App extends React.Component {
               <Stack.Screen name="ChangeUserPasswordScreen" component={ChangePasswordScreen} options={{headerShown: false}}/>
               <Stack.Screen name="AskCreatePet" component={AskCreatePetScreen} options={{headerBackVisible: false, title: '', headerStyle: {backgroundColor: colors.primary}}} />
               <Stack.Screen name="CreatePet" component={CreatePetScreen} options={{title: '', headerShown: false }} initialParams={{ userInfo:{}, initialSetup:false, initPetType:'DOG'}} />
-              <Stack.Screen name="EditPetDetails" component={EditPetDetailsScreen} options={{title: 'Editar Información', headerShown: true, headerBackTitle: 'Atrás' , headerTintColor: colors.white, headerStyle: {backgroundColor: colors.primary}}}/>
-              <Stack.Screen name="ViewPet" component={ViewPetDetailsScreen} options={{title: 'Mascota', headerShown: true, headerBackTitle: 'Atrás', headerTintColor: colors.white, headerStyle: {backgroundColor: colors.primary}}} />
+              <Stack.Screen name="EditPetDetails" component={EditPetDetailsScreen} options={{ headerShown: false }}/>
+              <Stack.Screen name="ViewPet" component={ViewPetDetailsScreen} options={{ headerShown: false }} />
               <Stack.Screen name="ImageSelectorScreen" component={ImageSelectorScreen} options={{ headerShown: false}} />
               <Stack.Screen name="FosteringVolunteerProfile" component={FosteringVolunteerProfileScreen} options={{headerShown: false}} />
               <Stack.Screen name="FosteringVolunteerProfileSettings" component={FosteringVolunteerProfileSettingsScreen} options={{headerShown: false}} />
               <Stack.Screen name="ReportListFilter" component={ReportListFilterScreen} options={{headerShown: false}} />
-              <Stack.Screen name="ReportView" component={ReportViewScreen} options={{title: 'Reporte', headerShown: true, headerBackTitle: 'Atrás' , headerTintColor: colors.white, headerStyle: {backgroundColor: colors.primary}}} />
-              <Stack.Screen name="EditReportScreen" component={EditReportScreen} options={{title: 'Editar Reporte', headerShown: true, headerBackTitle: 'Atrás' , headerTintColor: colors.white, headerStyle: {backgroundColor: colors.primary}}} />
+              <Stack.Screen name="ReportView" component={ReportViewScreen} options={{headerShown: false}} />
+              <Stack.Screen name="EditReportScreen" component={EditReportScreen} options={{ headerShown: false }} />
               <Stack.Screen name="FaceRecognitionResults" component={FaceRecognitionResultsScreen} options={{headerShown: false}} />
             </Stack.Navigator>
           </NavigationContainer>;
